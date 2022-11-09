46101e7feb83e63b4561d0cd04ed283bf8c9d88d
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2022-11-09 03:08:16 -0500

    new request to update patient details

--- backend/index.js
+++ backend/index.js
@@ -25,13 +25,13 @@ app.get("/", (req, res) => {
   );
 });
 
-//below route is to get a list of all patients
+//to get a list of all patients
 app.get("/patients", async (req, res) => {
   console.log(typeof patients);
   res.json({ data: patients });
 });
 
-//below request adds a patient to the list of patients
+//to add a patient to the list of patients
 app.put("/patients", async (req, res) => {
   console.log("receiving");
   console.log(req.body);
@@ -51,21 +51,43 @@ app.put("/patients", async (req, res) => {
   res.json({ body: patients });
 });
 
-//below route fetches details of a single patient by id
+//to fetch details of a single patient by id
 app.get("/patient/:id", (req, res) => {
   const patient = patients.find((obj) => obj.id === req.params.id);
   console.log(patient);
   res.json({ data: patient });
 });
 
+//to add new fields or update fields in a patient by id
+app.post("/patient/:id", (req, res) => {
+  console.log(typeof req.body)
+  const newPatients = patients.map(patient => {
+    return (patient.id===req.params.id) ? {...patient, ...req.body} : patient
+  })
+
+  fs.writeFile(__dirname + "/" + "patient-api.json", JSON.stringify(newPatients), err=>{
+    if(err) throw err;
+    console.log("object has been updated");
+  })
+
+  res.json({data: newPatients, message:`patient with id ${req.params.id} has been updated`})
+})
+
+
+//to delete a patient by its id
 app.delete("/patient/:id", (req, res) => {
   const newPatients = patients.filter(
     (patient) => patient.id !== req.params.id
   );
   console.log(newPatients);
+
+  fs.writeFile(__dirname + "/" + "patient-api.json", JSON.stringify(newPatients), err=>{
+    if(err) throw err;
+    console.log("object has been deleted");
+  })
+
   res.json({
-    oldData: patients,
-    newData: newPatients,
+    newPatients,
     message: `The patient with ID - ${req.params.id} has been deleted`,
   });
 });
