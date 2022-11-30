a913d56a8d5a422b4f027f3dabfaa8fa9b7e7224
Ashish's ASUS laptop <hrushikeshmohanty90@gmail.com>
2022-11-30 15:18:10 -0500

    get doctor by id added

--- server.js
+++ server.js
@@ -20,65 +20,81 @@ console.log('Connected to PlanetScale!')
 //console.log(process.env.DATABASE_URL)
 
 //checking if database is connected
-connection.connect((err)=>{
-    if(err){
+connection.connect((err) => {
+    if (err) {
         console.log(err)
-    }else{
+    } else {
         console.log("Connected!!!")
     }
 })
 
 //get details of all doctors
-app.get("/",(request,response)=>{
-    connection.query("select * from Doctor",function(err,result,fields){
-        if (err){
-            response.json({message: "Cannot get List of doctors"})
+app.get("/doctors", (request, response) => {
+    connection.query("select * from Doctor", function (err, result, fields) {
+        if (err) {
+            response.json({ message: "Cannot get List of doctors" })
         }
-        else{
-            response.json({data: result})
+        else {
+            response.json({ data: result })
             console.log(JSON.stringify(result))
         }
     })
 })
 
 
-//add new doctor with all details
-app.post("/post-doctor", (request,response)=>{
-    const Id = uuid.v4()
-    const doctorDb = {Id, ...request.body}
-
-    const { First_name, Last_name, Email, Contact_Number, Qualification, Specialization, Profile_Picture} = {...request.body}
-    console.log(request.body)
-    
-    connection.query('insert into Doctor values(?,?,?,?,?,?,?,?)',[Id,First_name,Last_name,Email,Contact_Number,Qualification,Specialization,Profile_Picture],
-    (err,res)=>{
-        if (err) {
-            throw err
-            response.json({message: "error in creating new Doctor"})
+//get details doctor by id
+app.get("/doctors/:id", (request, response) => {
+    const id = request.params.id
+    console.log(id)
+    connection.query(`select * from Doctor where Id = ?`, [id], (error, result) => {
+        if (error) {
+            throw error
+            response.json({ message: "error in creating new Doctor" })
         }
-        console.log(res)
-        response.json({data: doctorDb, message: "New doctor has been created"})
-
+        console.log(result)
+        response.json({ data: result })
     })
+});
+
+
+//add new doctor with all details
+app.post("/doctors", (request, response) => {
+    const Id = uuid.v4()
+    const doctorDb = { Id, ...request.body }
+
+    const { First_name, Last_name, Email, Contact_Number, Qualification, Specialization, Profile_Picture } = { ...request.body }
+    console.log(request.body)
+
+    connection.query('insert into Doctor values(?,?,?,?,?,?,?,?)', [Id, First_name, Last_name, Email, Contact_Number, Qualification, Specialization, Profile_Picture],
+        (err, res) => {
+            if (err) {
+                throw err
+                response.json({ message: "error in creating new Doctor" })
+            }
+            console.log(res)
+            response.json({ data: doctorDb, message: "New doctor has been created" })
+
+        })
 })
+
+
 
 //delete doctor by id
-app.delete("/delete-doctor",(request,response)=>{
-    const Id = request.body.Id
+app.delete("/doctors/:id", (request, response) => {
+    const Id = request.params.id
 
-    connection.query('delete from Doctor where Id = ?',[Id],(error,result)=>{
-        if(error) throw error
+    connection.query('delete from Doctor where Id = ?', [Id], (error, result) => {
+        if (error) throw error
         console.log(result)
-        response.json({data: `Doctor with Id ${Id} has been deleted`})
+        response.json({ data: `Doctor with Id ${Id} has been deleted` })
     })
 
 })
 
-app.listen(3000,(err)=>{
-    if(err)
-    {
+app.listen(3000, (err) => {
+    if (err) {
         console.log(err)
-    }else{
+    } else {
         console.log("listening on port 3000")
     }
 })
